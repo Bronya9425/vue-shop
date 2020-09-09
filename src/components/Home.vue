aeb198eec15bcf24057fc559384a8fa7066f5acb
Bronya9425 <1021600534@qq.com>
2020-09-09 22:55:36 +0800

    改良了页面布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,95 @@
 <template>
-    <div>
-        <el-button round @click="logout">圆角按钮</el-button>
-    </div>
+  <el-container>
+    <!-- 头部 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt />
+        <span>管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">用户退出</el-button>
+    </el-header>
+    <!-- 页面 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单栏 -->
+        <el-menu background-color="#b3c0d1" text-color="black" active-text-color="#00FFFF">
+          <el-submenu :index="item.id" v-for="item in menulist" :key="item.id">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 次级菜单 -->
+            <el-menu-item-group>
+            <!-- 每组列表有符合的内容 -->
+              <template slot="title" class="el-icon-document">
+                <i class="el-icon-s-order"></i>
+                {{item.children[0].authName}}
+              </template>
+              <el-menu-item index="1-1" class="el-icon-coin">{{item.authName.slice(0,2)}}{{item.id}}</el-menu-item>
+              <el-menu-item index="1-2" class="el-icon-coin">{{item.authName.slice(0,2)}}{{item.id + 1}}</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 左侧菜单栏数据
+      menulist: [],
+      index: []
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
     }
   }
 }
 </script>
 
 <style lang='less' scoped>
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  display: flex;
+  justify-content: space-between;
+  padding: 0;
+  img {
+    height: 48px;
+  }
+  > div {
+    font-size: 28px;
+    display: flex;
+    align-items: center;
+  }
+}
 
+.el-aside {
+  background-color: #d3dce6;
+}
+
+.el-main {
+  background-color: #e9eef3;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'// 服务器地址
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须返回config
+  return config
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
