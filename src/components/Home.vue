a797d27c7876a92e964a5947cd77d301d907ba45
Bronya9425 <1021600534@qq.com>
2020-09-10 13:42:10 +0800

    基本页面布局完善

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,23 +11,24 @@
     <!-- 页面 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">
-        <!-- 侧边栏菜单栏 -->
-        <el-menu background-color="#b3c0d1" text-color="black" active-text-color="#00FFFF">
+      <el-aside :width="iscollapse ? '64px' : '200px'">
+        <!-- 侧边栏内容 -->
+        <div class="tog-button" @click="checkis">|||</div>
+        <el-menu background-color="#b3c0d1" text-color="black" active-text-color="#00FFFF" unique-opened :collapse="iscollapse">
           <el-submenu :index="item.id" v-for="item in menulist" :key="item.id">
             <template slot="title">
-              <i class="el-icon-menu"></i>
+              <i :class="index[item.id]"></i>
               <span>{{item.authName}}</span>
             </template>
             <!-- 次级菜单 -->
             <el-menu-item-group>
             <!-- 每组列表有符合的内容 -->
               <template slot="title" class="el-icon-document">
-                <i class="el-icon-s-order"></i>
+                <i :class="index[item.id]"></i>
                 {{item.children[0].authName}}
               </template>
-              <el-menu-item index="1-1" class="el-icon-coin">{{item.authName.slice(0,2)}}{{item.id}}</el-menu-item>
-              <el-menu-item index="1-2" class="el-icon-coin">{{item.authName.slice(0,2)}}{{item.id + 1}}</el-menu-item>
+              <el-menu-item index="1-1" class="el-icon-caret-right">{{item.authName.slice(0,2)}}{{item.id}}</el-menu-item>
+              <el-menu-item index="1-2" class="el-icon-caret-right">{{item.authName.slice(0,2)}}{{item.id + 1}}</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
         </el-menu>
@@ -44,7 +45,14 @@ export default {
     return {
       // 左侧菜单栏数据
       menulist: [],
-      index: []
+      index: {
+        125: 'el-icon-user-solid',
+        103: 'el-icon-s-opportunity',
+        101: 'el-icon-money',
+        102: 'el-icon-notebook-2',
+        145: 'el-icon-user-solid'
+      },
+      iscollapse: false
     }
   },
   created () {
@@ -60,6 +68,9 @@ export default {
       if (res.meta.status !== 200) return this.message.error(res.meta.msg)
       this.menulist = res.data
       console.log(res)
+    },
+    checkis () {
+      this.iscollapse = !this.iscollapse
     }
   }
 }
@@ -87,9 +98,21 @@ export default {
 
 .el-aside {
   background-color: #d3dce6;
+  .el-menu{
+    border-right: none
+  }
 }
 
 .el-main {
   background-color: #e9eef3;
+}
+
+.tog-button {
+  background-color: #646464;
+  font-size: 10px;
+  line-height: 25px;
+  text-align: center;
+  letter-spacing: 0.3em;
+  cursor: pointer;
 }
 </style>
